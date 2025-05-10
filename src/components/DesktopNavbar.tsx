4f02a6ad5e15914bb7e546ddcfb3a97c4252b62d
NgocHai77 <ngochai070700@gmail.com>
2025-05-10 12:32:52 +0700

    project completed

---
+++ src/components/DesktopNavbar.tsx
@@ -0,0 +1,50 @@
+import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import ModeToggle from "./ui/ModeToggle";
+import { currentUser } from "@clerk/nextjs/server";
+
+async function DesktopNavbar() {
+  const user = await currentUser();
+
+  return (
+    <div className="hidden md:flex items-center space-x-4">
+      <ModeToggle />
+
+      <Button variant="ghost" className="flex items-center gap-2" asChild>
+        <Link href="/">
+          <HomeIcon className="w-4 h-4" />
+          <span className="hidden lg:inline">Trang Chủ</span>
+        </Link>
+      </Button>
+
+      {user ? (
+        <>
+          <Button variant="ghost" className="flex items-center gap-2" asChild>
+            <Link href="/notifications">
+              <BellIcon className="w-4 h-4" />
+              <span className="hidden lg:inline">Thông Báo</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" className="flex items-center gap-2" asChild>
+            <Link
+              href={`/profile/${
+                user.username ?? user.emailAddresses[0].emailAddress.split("@")[0]
+              }`}
+            >
+              <UserIcon className="w-4 h-4" />
+              <span className="hidden lg:inline">Trang Cá Nhân</span>
+            </Link>
+          </Button>
+          <UserButton />
+        </>
+      ) : (
+        <SignInButton mode="modal">
+          <Button variant="default">Đăng Nhập</Button>
+        </SignInButton>
+      )}
+    </div>
+  );
+}
+export default DesktopNavbar;
